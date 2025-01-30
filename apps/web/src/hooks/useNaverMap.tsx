a14fca0e24f7791b7025764c0467d0f6df62a835
eepy <eepy2.23@gmail.com>
2025-01-30 23:20:28 +0900

    feat(web): add map controls, refactor code

--- apps/web/src/hooks/useNaverMap.tsx
+++ apps/web/src/hooks/useNaverMap.tsx
@@ -1,11 +1,24 @@
 import { useEffect, useRef } from "react";
 
+const DEFAULT_POSITION = {
+  latitude: 37.3595704,
+  longitude: 127.105399,
+};
+
+const MAP_ZOOM_LEVEL = 16;
+
 const createMap = (map: typeof naver.maps, container: HTMLDivElement) => {
-  const center = new map.LatLng(37.3595704, 127.105399);
+  const center = new map.LatLng(
+    DEFAULT_POSITION.latitude,
+    DEFAULT_POSITION.longitude
+  );
 
   return new map.Map(container, {
     center,
-    zoom: 16,
+    zoom: MAP_ZOOM_LEVEL,
+    zoomControl: true, // 줌 컨트롤
+    disableDoubleTapZoom: true, // 더블탭 줌 허용
+    mapTypeControl: true, // 지도 유형 컨트롤
   });
 };
 
@@ -17,7 +30,9 @@ const useNaverMap = () => {
     const container = mapRef.current;
     if (!container) return;
 
-    createMap(naver.maps, container);
+    const map = createMap(naver.maps, container);
+
+    mapInstance.current = map;
 
     return () => {
       mapInstance.current = null;
@@ -26,6 +41,7 @@ const useNaverMap = () => {
 
   return {
     mapRef,
+    map: mapInstance.current,
   };
 };
 
