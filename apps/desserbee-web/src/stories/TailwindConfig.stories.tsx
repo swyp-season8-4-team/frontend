61095ddb6e8c6495ab6a998fff70cbab355e544f
eepy <eepy2.23@gmail.com>
2025-01-31 22:57:07 +0900

    feat(web): Add TailwindConfig stories for theme color and spacing testing

---
+++ apps/desserbee-web/src/stories/TailwindConfig.stories.tsx
@@ -0,0 +1,53 @@
+import type { Meta, StoryObj } from "@storybook/react";
+interface TailwindConfigestProps {
+  className?: string;
+  text?: string;
+}
+
+const TailwindConfigTest = ({ className, text }: TailwindConfigestProps) => {
+  return (
+    <div className={className + "bg-red-400 w-60 h-60"}>
+      <div className="bg-black w-full h-full dark:text-white">{text}</div>
+    </div>
+  );
+};
+const meta = {
+  title: "Theme/TailwindConfig",
+  component: TailwindConfigTest,
+  parameters: {
+    layout: "fullscreen",
+  },
+} satisfies Meta<typeof TailwindConfigTest>;
+
+export default meta;
+type Story = StoryObj<typeof meta>;
+
+// color
+export const Primary: Story = {
+  args: {
+    className: "text-primary ",
+    text: "적용: className='text-primary'",
+  },
+};
+
+export const Secondary: Story = {
+  args: {
+    className: "text-secondary ",
+    text: "적용: className='text-secondary'",
+  },
+};
+
+export const Accent: Story = {
+  args: {
+    className: "text-accent ",
+    text: "적용: className='text-accent'",
+  },
+};
+
+// 패딩 (spacing)
+export const Base_PX_PY: Story = {
+  args: {
+    className: "text-primary px-base py-base ",
+    text: "적용: className='px-base py-base'",
+  },
+};
