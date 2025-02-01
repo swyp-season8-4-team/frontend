a09c548cab28a479d49437f384b61ed7ec82d997
eepy <eepy2.23@gmail.com>
2025-02-02 03:12:42 +0900

    comment(desserbee-web): border-radius

--- apps/desserbee-web/src/app/[lang]/(user)/map/_components/KakaoMap.tsx
+++ apps/desserbee-web/src/app/[lang]/(user)/map/_components/KakaoMap.tsx
@@ -9,8 +9,11 @@ interface KakoMapProps {
 export function KakaoMap({ children }: KakoMapProps) {
   const { mapRef } = useKakaoMap();
 
+  //TODO: rounded-20px tailwind.config.ts에 등록
   return (
-    <div ref={mapRef} className="w-full h-[574px] rounded-[20px] relative">
+    <div
+      ref={mapRef}
+      className="w-full h-[574px] my-[26px] rounded-[20px] relative">
       {children}
     </div>
   );
