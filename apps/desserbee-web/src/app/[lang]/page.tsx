445e1c08e795a60a166eed330cfba35e75d05004
eepy <eepy2.23@gmail.com>
2025-02-01 21:37:19 +0900

    fix(desserbee-seb): rebase error

--- apps/desserbee-web/src/app/[lang]/page.tsx
+++ apps/desserbee-web/src/app/[lang]/page.tsx
@@ -1,14 +1,10 @@
 import { KakaoMap } from "@/components/KakaoMap";
 
 export default async function RootPage() {
-<<<<<<< HEAD
-  return <div>RootPage</div>;
-=======
   return (
     <div>
       RootPage
       <KakaoMap />
     </div>
   );
->>>>>>> e78b782 (feat(web): Integrate Kakao Maps with custom marker and map controls)
 }
