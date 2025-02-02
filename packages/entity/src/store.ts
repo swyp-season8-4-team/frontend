fdcd8112467112a1e96726ce3d8c081f602c1a91
eepy <eepy2.23@gmail.com>
2025-02-03 04:25:27 +0900

    feat(entity): Add store-related data interfaces

---
+++ packages/entity/src/store.ts
@@ -0,0 +1,69 @@
+export interface StoreMapData {
+  id: number;
+  name: string;
+  address: string;
+  latitude: number;
+  longitude: number;
+}
+
+export interface StoreSummaryData {
+  id: number;
+  name: string;
+  averageRating: number;
+  tags: string[];
+  storeImages: string[];
+  address: string;
+  operatingHours: string;
+  closingDays: string;
+  phone: string;
+  storeLink: string;
+  animalYn: boolean;
+  tumblerYn: boolean;
+  parkingYn: boolean;
+}
+
+export interface StoreEventData {
+  id: number;
+  title: string;
+  description: string;
+  startDate: string;
+  endDate: string;
+}
+
+export interface StoreCouponData {
+  title: string;
+  description: string;
+  expiryDate: string;
+}
+
+export type StoreTag = "베이커리" | "루프탑 있음" | "애완동물 동반 가능"; // TODO: 전체 카테고리 정리하기
+
+export interface StoreDetailData {
+  id: number;
+  name: string;
+  address: string;
+  operatingHours: string;
+  closingDays: string;
+  phone: string;
+  storeLink: string;
+  animalYn: boolean;
+  tumblerYn: boolean;
+  parkingYn: boolean;
+  averageRating: number;
+  events: StoreEventData[];
+  menus: null; // TODO: 아직 api 명세서에 없음
+  coupons: StoreCouponData[];
+  storeImages: string[];
+  eventImages: string[];
+  menuImages: string[];
+  storeReviews: string[]; // TODO: 아직 api 명세서에 없음
+  tags: StoreTag[];
+}
+
+export interface UserSavedStoreData {
+  id: number;
+  name: string;
+  address: string;
+  thumbnail: string;
+  savedAt: string;
+}
