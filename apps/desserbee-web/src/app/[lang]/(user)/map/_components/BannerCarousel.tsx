ec651477465851b174257f5c15f921b8c343aecb
eepy <eepy2.23@gmail.com>
2025-02-05 02:19:21 +0900

    fix(desserbee-web): BannerCarousel style

--- apps/desserbee-web/src/app/[lang]/(user)/map/_components/BannerCarousel.tsx
+++ apps/desserbee-web/src/app/[lang]/(user)/map/_components/BannerCarousel.tsx
@@ -1,19 +1,16 @@
-"use client";
-import {
-  Carousel,
-  CarouselContent,
-  CarouselItem,
-  Autoplay,
-} from "@repo/ui/components/carousel";
-import { BANNERS } from "../_consts/banner";
+'use client';
+import { Carousel, CarouselContent, CarouselItem, Autoplay } from '@repo/ui/components/carousel';
+import { BANNERS } from '../_consts/banner';
+import bee from '../_assets/svg/logo-bee.svg';
+import Image from 'next/image';
 
 export function BannerCarousel() {
   return (
     <Carousel
-      className="w-full rounded-[20px] overflow-x-hidden"
+      className="rounded-[20px] w-full overflow-x-hidden"
       opts={{
-        align: "start",
-        containScroll: "trimSnaps",
+        align: 'start',
+        containScroll: 'trimSnaps',
         loop: true,
       }}
       plugins={[
@@ -21,15 +18,18 @@ export function BannerCarousel() {
           delay: 3000,
         }),
       ]}>
-      <CarouselContent className="-ml-2 w-full">
+      <CarouselContent className="mb-[13px] w-full">
         {BANNERS.map((banner) => (
           <CarouselItem
-            key={banner.content}
-            className={`pl-8 pr-3 flex items-center py-[11px] justify-between shadow-2xl rounded-[20px] w-full`}>
-            <div className="font-semibold text-3xl whitespace-pre-line text-nowrap">
-              {banner.content}
+            className={`relative flex justify-between items-center bg-secondary px-[27px] py-6 rounded-base w-[calc(100vw-16px)] basis-1/${BANNERS.length}`}
+            key={banner.content}>
+            <div className="flex">
+              <Image src={bee} className="mr-[18px]" width={69} height={68} alt="logo" />
+              <div className="font-semibold text-3xl text-nowrap -tracking-[3%] whitespace-pre-line">
+                {banner.content}
+              </div>
             </div>
-            <button className="h-full p-10 font-semibold text-lg rounded-[20px] shadow-lg text-nowrap">
+            <button className="right-[15px] bottom-[15px] absolute flex justify-center items-center bg-[#AA6120] px-[27px] py-[9px] rounded-[100px] h-11 font-semibold text-[22px] text-nowrap text-white">
               {banner.btnContent}
             </button>
           </CarouselItem>
