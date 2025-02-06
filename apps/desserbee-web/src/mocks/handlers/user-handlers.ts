f50b1c261a2da33d353d7ee7ec4cf2132ff8e54a
eepy <eepy2.23@gmail.com>
2025-02-07 00:59:09 +0900

    feat(desserbee-web): store mock data implement

--- apps/desserbee-web/src/mocks/handlers/user-handlers.ts
+++ apps/desserbee-web/src/mocks/handlers/user-handlers.ts
@@ -1,19 +1,19 @@
-import { http, HttpResponse } from "msw";
+import { http, HttpResponse } from 'msw';
 
 export const userHandlers = [
-  http.get("https://example.com/user", () => {
+  http.get('https://example.com/user', () => {
     return HttpResponse.json({
-      id: "c7b3d8e0-5e0b-4b0f-8b3a-3b9f4b3d3b3d",
-      firstName: "John",
-      lastName: "Maverick",
+      id: 'c7b3d8e0-5e0b-4b0f-8b3a-3b9f4b3d3b3d',
+      firstName: 'John',
+      lastName: 'Maverick',
     });
   }),
 
-  http.get("http://localhost:3000/user", () => {
+  http.get(`${process.env.NEXT_PUBLIC_APP_HOST}/api/user`, () => {
     return HttpResponse.json({
-      id: "c7b3d8e0-5e0b-4b0f-8b3a-3b9f4b3d3b3d",
-      firstName: "John",
-      lastName: "Maverick",
+      id: 'c7b3d8e0-5e0b-4b0f-8b3a-3b9f4b3d3b3d',
+      firstName: 'John',
+      lastName: 'Maverick',
     });
   }),
 ];
