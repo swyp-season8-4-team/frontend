ad50da50c086c6d16e5773a00ccc2c0b2e0b5e2e
eepy <eepy2.23@gmail.com>
2025-02-07 15:50:02 +0900

    feat(desserbee-web): worker browser add option(onUnhandledRequest:bypass)

--- apps/desserbee-web/src/mocks/index.ts
+++ apps/desserbee-web/src/mocks/index.ts
@@ -1,14 +1,16 @@
 async function initMSW() {
-  if (typeof window === "undefined") {
-    const { server } = await import("./server");
+  if (typeof window === 'undefined') {
+    const { server } = await import('./server');
 
-    console.log("server mock");
+    console.log('server mock');
     server.listen();
   } else {
-    const { worker } = await import("./browser");
+    const { worker } = await import('./browser');
 
-    console.log("broswer mock");
-    await worker.start();
+    console.log('broswer mock');
+    await worker.start({
+      onUnhandledRequest: 'bypass', // 모든 미처리 요청 무시
+    });
   }
 }
 
