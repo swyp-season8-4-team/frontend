caa0564759c5e6fedb31234781b602564df36110
hans <hans@hanss-MacBook-Pro.local>
2025-02-09 20:52:48 +0900

    [desser-bee] feat: 디저트 메이트 레이아웃 추가

--- apps/desserbee-web/src/app/[lang]/(user)/(search)/layout.tsx
+++ apps/desserbee-web/src/app/[lang]/(user)/(search)/layout.tsx
@@ -1,23 +1,12 @@
 import { Header } from "@repo/design-system/components/Header";
 import type { WithChildren } from "@repo/ui/index";
-import localFont from "next/font/local";
 import { SearchBarWithHook } from "./_components/SearchBarWithHook";
-
-const recipeKorea = localFont({
-  src: [
-    {
-      path: '../../../fonts/RecipeKorea.ttf',
-      weight: '400',
-      style: 'normal',
-    },
-  ],
-  variable: '--font-recipe-korea',
-});
+import { recipeKorea } from "@/app/fonts";
 
 export default function UserSearchLayout({ children }: WithChildren) {
   return (
     <>
-      <Header fontClass={recipeKorea.className}>
+      <Header title="디저비" fontClass={recipeKorea.className}>
         <SearchBarWithHook />
       </Header>
       {children}
--- packages/design-system/src/components/Header.tsx
+++ packages/design-system/src/components/Header.tsx
@@ -1,21 +1,22 @@
+import type { WithChildren } from '@repo/ui/index';
 import { cn } from '@repo/ui/lib/utils';
-import type { ReactNode } from 'react';
-interface HeaderProp {
+
+interface HeaderProp extends WithChildren {
+  title: string;
   fontClass: string;
-  children: ReactNode;
 }
 
-export function Header({ fontClass, children }: HeaderProp) {
+export function Header({ title, fontClass, children }: HeaderProp) {
   return (
     <header className="sticky top-0 bg-primary px-base pt-8 w-full text-[22px]">
-      <div
+      <h1
         className={cn(
           fontClass,
           'text-3xl text-white leading-10 -tracking-[3%]',
         )}
       >
-        디저비
-      </div>
+        {title}
+      </h1>
       {children}
     </header>
   );
