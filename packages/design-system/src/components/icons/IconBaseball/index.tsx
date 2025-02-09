2b7309c45f65b9ba6ca201c165e38c398d70a0da
eepy <eepy2.23@gmail.com>
2025-02-09 22:44:03 +0900

    fix(design-system): icon viewbox updated

--- packages/design-system/src/components/icons/IconBaseball/index.tsx
+++ packages/design-system/src/components/icons/IconBaseball/index.tsx
@@ -5,7 +5,7 @@ export default function IconBaseball({
   ...props
 }: SVGProps) {
   return (
-    <Icon size={size} {...props}>
+    <Icon viewBox="0 0 16 16" size={size} {...props}>
       <path
         d="M8 16C12.4183 16 16 12.4183 16 8C16 3.58172 12.4183 0 8 0C3.58172 0 0 3.58172 0 8C0 12.4183 3.58172 16 8 16Z"
         fill="currentColor"
--- packages/design-system/src/components/icons/IconHome/index.tsx
+++ packages/design-system/src/components/icons/IconHome/index.tsx
@@ -2,7 +2,7 @@ import Icon, { IconSize, type SVGProps } from '..';
 
 export default function IconHome({ size = IconSize.m, ...props }: SVGProps) {
   return (
-    <Icon size={size} {...props}>
+    <Icon viewBox="0 0 18 18" size={size} {...props}>
       <path
         d="M1 16L1.04432 6.11399C1.04432 5.61271 1.30138 5.13612 1.72687 4.82384L7.93185 0.345147C8.57008 -0.115049 9.46538 -0.115049 10.1125 0.345147L16.3175 4.81562C16.7518 5.12789 17 5.60449 17 6.11399V16"
         fill="currentColor"
