f3c1713ad976ad947abf8c8b8921e52172d1cd7e
eepy <eepy2.23@gmail.com>
2025-02-12 22:24:52 +0900

    fix(design-system): add className prop to allow external styling customization

--- packages/design-system/src/components/Tag/index.tsx
+++ packages/design-system/src/components/Tag/index.tsx
@@ -3,23 +3,16 @@ import { cn } from '@repo/ui/lib/utils';
 
 interface TagProps {
   children: ReactNode;
-  isSelected?: boolean;
-  disabled?: boolean;
+  className?: string;
 }
 
-export function Tag({
-  children,
-  isSelected = false,
-  disabled = false,
-}: TagProps) {
+export function Tag({ children, className }: TagProps) {
   return (
     <button
       className={cn(
-        'bg-white shadow-base px-4 py-2 rounded-[48.78px] min-w-[104px] font-medium text-lg select-none',
-        isSelected && 'bg-primary text-white',
-        !isSelected && 'bg-white text-[#393939]',
+        'shadow-base px-4 py-2 rounded-[48.78px]  bg-white',
+        className,
       )}
-      disabled={disabled}
     >
       {children}
     </button>
