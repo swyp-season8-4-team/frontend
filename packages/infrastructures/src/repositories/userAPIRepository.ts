e0af69890c59cfa8a51e32f6f4466ca77fc96e93
hans <hans@hanss-MacBook-Pro.local>
2025-02-15 23:09:40 +0900

    [deserbee-web] feat: user api 추가

--- packages/infrastructures/src/repositories/userAPIRepository.ts
+++ packages/infrastructures/src/repositories/userAPIRepository.ts
@@ -1,5 +1,5 @@
 import type { BaseRequestData } from '@repo/entity/src/appMetadata';
-import type { User, UserRepository } from '@repo/entity/src/user';
+import type { NicknameValidationRequestData, TargetUser, User, UserRepository } from '@repo/entity/src/user';
 import type { RawUser } from '@repo/api/src/desserbee-web/user';
 import fetch from '@repo/api/src/fetch';
 import APIRepository from './apiRepository';
@@ -21,10 +21,29 @@ export default class UserAPIRepository extends APIRepository implements UserRepo
     return response;
   }
 
-  async get({ authorization }: BaseRequestData<void>): Promise<User> {
+  async getMe({ authorization }: BaseRequestData<void>): Promise<User> {
     const url = `${this.endpoint}/users/me`;
 
     const response = await fetch<void, RawUser>({
+      ...(authorization && {
+        headers: {
+          Authorization: authorization,
+        },
+      }),
+      method: 'GET',
+      url,
+    });
+
+    return response;
+  }
+
+  async getTarget({ authorization, data }: BaseRequestData<{ id: string; }>): Promise<TargetUser> {
+    if (!data) {
+      throw new Error('data is required');
+    }
+    const url = `${this.endpoint}/users/${data.id}`;
+
+    const response = await fetch<void, TargetUser>({
       ...(authorization && {
         headers: {
           Authorization: authorization,
@@ -67,5 +86,9 @@ export default class UserAPIRepository extends APIRepository implements UserRepo
       method: 'DELETE',
       url,
     });
+  }
+
+  validateNickname(data: BaseRequestData<NicknameValidationRequestData>): Promise<void> {
+    throw new Error('Method not implemented.');
   }
 }
