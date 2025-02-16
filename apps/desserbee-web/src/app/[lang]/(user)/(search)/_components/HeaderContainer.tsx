e496d4b8b35e19a13566a034647a2969526bbcde
eepy <eepy2.23@gmail.com>
2025-02-16 18:00:14 +0900

    fix(desserbee-web): Modify conditional rendering logic for header container

--- apps/desserbee-web/src/app/[lang]/(user)/(search)/_components/HeaderContainer.tsx
+++ apps/desserbee-web/src/app/[lang]/(user)/(search)/_components/HeaderContainer.tsx
@@ -11,11 +11,14 @@ interface HeaderContainer {
 export function HeaderContainer({ fontClass }: HeaderContainer) {
   const pathname = usePathname();
 
-  const normalizedPath = pathname.split('/', 4)[3];
-  const currentPathName = `/map/${normalizedPath}`;
-  const isStoreDetail = /^\/map\/\d+$/.test(currentPathName);
+  const isStoreDetailPage = () => {
+    const segments = pathname.split('/');
+    return (
+      segments.includes('map') && segments.length > segments.indexOf('map') + 1 // 경로에 map 포함한지 확인, /map/[storeId](상세페이지)인지 확인
+    );
+  };
 
-  if (isStoreDetail) return null;
+  if (isStoreDetailPage()) return null;
 
   return (
     <Header title="디저비" fontClass={fontClass}>
