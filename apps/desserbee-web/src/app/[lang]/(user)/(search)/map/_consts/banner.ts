0dfb871d92f3a90f979ede4ce047dbffef5f3c84
eepy <eepy2.23@gmail.com>
2025-02-17 13:49:02 +0900

    style(desserbee-web): 배너 아이템 아이콘, 배경 다르게 나오도록 수정

--- apps/desserbee-web/src/app/[lang]/(user)/(search)/map/_consts/banner.ts
+++ apps/desserbee-web/src/app/[lang]/(user)/(search)/map/_consts/banner.ts
@@ -1,18 +1,35 @@
+import bee from '@/app/[lang]/(user)/(search)/map/_assets/svg/logo-bee.svg';
+import map from '@/app/[lang]/(user)/(search)/map/_assets/svg/banner-map.png';
+import store from '@/app/[lang]/(user)/(search)/map/_assets/svg/banner-store.png';
+import ticket from '@/app/[lang]/(user)/(search)/map/_assets/svg/banner-ticket.png';
+
 export const BANNERS = [
   {
-    content: "우리 같이 디저트 맛집 갈 사람을\n찾아볼까요?",
-    btnContent: "디저트 메이트 찾기",
+    content: '우리 같이 디저트 맛집 갈 사람을\n찾아볼까요?',
+    bgColor: 'bg-[#FFE7A3]',
+    btnContent: '디저트 메이트 찾기',
+    imgSrc: bee,
+    path: '/community/mate',
   },
   {
-    content: "우리 동네 찐 리뷰를\n확인해 보세요!",
-    btnContent: "맛집 리뷰 확인하기",
+    content: '로그인 시 나의 취향에 딱 맞는\n디저트샵을 추천해줘요!',
+    bgColor: 'bg-[#FFDB75]',
+    btnContent: '로그인 하기',
+    imgSrc: store,
+    path: '/community/reviews',
   },
   {
-    content: "우리 동네 디저트 쿠폰\n받으러 갈까요?",
-    btnContent: "쿠폰 / 이벤트 확인하기",
+    content: '우리 동네 디저트 쿠폰\n받으러 갈까요?',
+    bgColor: 'bg-[#FFCF48]',
+    btnContent: '쿠폰 / 이벤트 보기',
+    imgSrc: ticket,
+    path: '/',
   },
   {
-    content: "로그인 하면 나의 취향에 딱 맞는\n디저트샵을 추천해줘요!",
-    btnContent: "추천 샵 확인하기",
+    content: '우리 동네 찐 리뷰를\n확인해 보세요!',
+    bgColor: 'bg-[#FFC635]',
+    btnContent: '디저트 리뷰 보기',
+    imgSrc: map,
+    path: '/sign-in',
   },
 ];
