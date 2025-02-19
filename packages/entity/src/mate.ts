e4d3bf3a48ee235ecc1273ac0f776327acb12d42
SeungHoon Jeong <seunghoon7561@gmail.com>
2025-02-19 22:19:52 +0900

    [desserbee-web] feat: mate쪽 api 정의 (#64)
    Co-authored-by: hans <hans@hanss-MacBook-Pro.local>

--- packages/entity/src/mate.ts
+++ packages/entity/src/mate.ts
@@ -29,6 +29,31 @@ export interface MateCreateRequest {
   mateCategoryId: string;
 }
 
+export interface MateApplyRequest {
+  mateId: string;
+  userId: string;
+}
+
+export interface MateLeaveRequest {
+  userUuid: string;
+}
+
+export interface MateAcceptRequest {
+  userId: string;
+  mateId: string;
+}
+
+export interface MateRejectRequest {
+  userId: string;
+  mateId: string;
+}
+
+export interface MateFireRequest {
+  creatorId: string;
+  userId: string;
+  mateId: string;
+}
+
 export interface MateRequest {
   id: string;
 }
@@ -48,6 +73,12 @@ export interface MateUpdateRequest extends MateRequest {
 }
 
 export interface MateRepository {
+  apply(data: BaseRequestData<MateApplyRequest>): Promise<unknown>; // 모임 참여
+  leave(data: BaseRequestData<MateLeaveRequest>): Promise<unknown>; // 모임 탈퇴
+  getMyTeamMembers(data: BaseRequestData<MateRequest>): Promise<Mate[]>; // 내 팀 멤버들 조회
+  acceptMyTeamMember(data: BaseRequestData<MateAcceptRequest>): Promise<unknown>; // 팀 멤버 수락
+  rejectMyTeamMember(data: BaseRequestData<MateRejectRequest>): Promise<unknown>; // 팀 멤버 거절
+  fireMyTeamMember(data: BaseRequestData<MateFireRequest>): Promise<unknown>; // 팀 멤버 추방
   getDetails(data: BaseRequestData<MateRequest>): Promise<Mate[]>; // 모임 상세 페이지
   create(data: BaseRequestData<MateCreateRequest>): Promise<Mate>; // 모임 생성
   delete(data: BaseRequestData<MateRequest>): Promise<void>; // 모임 삭제
--- packages/infrastructures/src/repositories/mateAPIRepository.ts
+++ packages/infrastructures/src/repositories/mateAPIRepository.ts
@@ -1,7 +1,12 @@
 import type { BaseRequestData } from '@repo/entity/src/appMetadata';
 import type {
   Mate,
+  MateAcceptRequest,
+  MateApplyRequest,
   MateCreateRequest,
+  MateFireRequest,
+  MateLeaveRequest,
+  MateRejectRequest,
   MateRepository,
   MateRequest,
   MateUpdateRequest,
@@ -17,6 +22,113 @@ export default class MateAPIRepository
 {
   private readonly mateConverter: MateConverter = new MateConverter();
 
+  async apply({ data }: BaseRequestData<MateApplyRequest>): Promise<unknown> {
+    if (!data) {
+      throw new Error('data is required');
+    }
+
+    const { mateId, userId } = data;
+
+    const response = await fetch<MateApplyRequest, unknown>({
+      data: {
+        mateId,
+        userId,
+      },
+      method: 'POST',
+      url: `${this.endpoint}/mates/${mateId}/apply`,
+    });
+
+    return response;
+  }
+
+  async leave({ data }: BaseRequestData<MateLeaveRequest>): Promise<unknown> {
+    if (!data) {
+      throw new Error('data is required');
+    }
+
+    const { userUuid } = data;
+
+    const response = await fetch<MateLeaveRequest, unknown>({
+      data: {
+        userUuid,
+      },
+      method: 'DELETE',
+      url: `${this.endpoint}/mates/leave`,
+    });
+
+    return response;
+  }
+
+  async getMyTeamMembers({ data }: BaseRequestData<MateRequest>): Promise<Mate[]> {
+    if (!data) {
+      throw new Error('data is required');
+    }
+
+    const { id } = data;
+    
+    const response = await fetch<void, RawMate[]>({
+      method: 'GET',
+      url: `${this.endpoint}/mates/${id}/members`,
+    });
+
+    return response.map((mate) => this.mateConverter.convertRawToMate(mate));
+  }
+
+  async acceptMyTeamMember({ data }: BaseRequestData<MateAcceptRequest>): Promise<unknown> {
+    if (!data) {
+      throw new Error('data is required');
+    }
+
+    const { userId, mateId } = data;
+
+    const response = await fetch<{ userUuid: string }, unknown>({
+      data: {
+        userUuid: userId,
+      },
+      method: 'PATCH',
+      url: `${this.endpoint}/mates/${mateId}/apply`,
+    });
+
+    return response;
+  }
+
+  async rejectMyTeamMember({ data }: BaseRequestData<MateRejectRequest>): Promise<unknown> {
+    if (!data) {
+      throw new Error('data is required');
+    }
+
+    const { userId, mateId } = data;
+
+    const response = await fetch<{ userUuid: string }, unknown>({
+      data: {
+        userUuid: userId,
+      },
+      method: 'DELETE',
+      url: `${this.endpoint}/mates/${mateId}/apply`,
+    });
+
+    return response;
+  }
+
+  async fireMyTeamMember({ data }: BaseRequestData<MateFireRequest>): Promise<unknown> {
+    if (!data) {
+      throw new Error('data is required');
+    }
+
+    const { creatorId, userId, mateId } = data;
+
+    const response = await fetch<{ creatorUuid: string; targetUuid: string }, unknown>({
+      data: {
+        creatorUuid: creatorId,
+        targetUuid: userId,
+      },
+      method: 'DELETE',
+      url: `${this.endpoint}/mates/${mateId}/members`,
+    });
+
+    return response;
+  }
+
   async getDetails({ data }: BaseRequestData<MateRequest>): Promise<Mate[]> {
     if (!data) {
       throw new Error('data is required');
@@ -24,7 +136,7 @@ export default class MateAPIRepository
 
     const response = await fetch<MateRequest, RawMate[]>({
       method: 'GET',
-      url: `${this.endpoint}/mates/${data.id}`,
+      url: `${this.endpoint}/mates/${data.id}/details`,
     });
 
     return response.map((mate) => this.mateConverter.convertRawToMate(mate));
