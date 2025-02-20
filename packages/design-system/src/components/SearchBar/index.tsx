303b21d98f61ada6e1b9d287d71f710eba9fd90c
eepy <eepy2.23@gmail.com>
2025-02-21 01:22:02 +0900

    fix(design-system): 서치바 반응형 수정

--- packages/design-system/src/components/SearchBar/index.tsx
+++ packages/design-system/src/components/SearchBar/index.tsx
@@ -35,12 +35,12 @@ export function SearchBar({
       <input
         type="text"
         value={searchTerm}
-        className="shadow-[2px_2px_5px_0px_rgba(0,0,0,0.1)] my-[9px] md:mt-[18px] md:mb-[14px] px-[42.32px] md:px-[53px] py-[9.17px] md:py-[22px] rounded-[60px] w-full md:text-[22px] text-sm"
+        className="shadow-[2px_2px_5px_0px_rgba(0,0,0,0.1)] my-[9px] md:mt-[18px] md:mb-[14px] py-[9.17px] md:py-[22px] pr-[20px] pl-[42.32px] md:pl-[53px] rounded-[60px] w-full md:text-[22px] text-sm"
         placeholder={placeHolder}
         onChange={handleChange}
         onKeyDown={handleKeyDown}
       />
-      <div className="top-[53%] left-6 absolute -translate-y-1/2">
+      <div className="top-1/2 left-[38px] md:left-[37.49px] absolute -translate-y-1/2">
         <IconSearch className="w-4 md:w-6 h-4 md:h-6 text-[#B4B4B4]" />
       </div>
     </form>
