906b26442f6cbad9369a300ef218e1f7dad4390d
eepy <eepy2.23@gmail.com>
2025-02-20 18:28:53 +0900

    사이드바 리스트 목록 반응형

--- apps/desserbee-web/src/app/[lang]/(user)/(search)/map/_components/sidebar/SidebarContainer/index.tsx
+++ apps/desserbee-web/src/app/[lang]/(user)/(search)/map/_components/sidebar/SidebarContainer/index.tsx
@@ -4,6 +4,7 @@ import IconFlower from '@repo/design-system/components/icons/IconFlower';
 import IconPin from '@repo/design-system/components/icons/IconPin';
 import { IconSize } from '@repo/design-system/components/icons';
 import IconPlus from '@repo/design-system/components/icons/IconPlus';
+import { cn } from '@repo/ui/lib/utils';
 interface sideBarProps {
   isSideBarOpen: boolean;
   handleSideBarClose: () => void;
@@ -36,46 +37,52 @@ export function SideBarContainer({
 
   return (
     <SideBar {...sideBarProps}>
-      <div>
-        <div className="text-[#393939] font-semibold text-[22px] w-full text-start p-2">
-          저장 리스트
+      <div className="leading-none">
+        <div className="flex p-2 w-full font-semibold text-[#393939] md:text-[22px] text-xs text-start">
+          <span>저장 리스트 &nbsp;</span>
+          <span>{savedList.length}</span>
         </div>
-        <div className="flex items-center my-[22px]">
-          <span className="rounded-sm border-[0.5px] border-[#D5D5D5] aspect-square p-2 mr-2">
-            <IconPlus className=" text-[#6F6F6F]" />
+        <div className="flex items-center my-[11.97px] md:my-[22px]">
+          <span className="flex justify-center items-center mr-2 border-[#D5D5D5] border-[0.5px] rounded-sm w-[20.7px] md:w-[30.45px] aspect-square">
+            <IconPlus className="w-[70%] h-[70%] text-[#6F6F6F]" />
           </span>
-          <span className="text-[#6F6F6F] text-[18px]">새 리스트 만들기</span>
+          <span className="text-[#6F6F6F] md:text-[18px] text-xs">
+            새 리스트 만들기
+          </span>
         </div>
         {savedList.map((saveListItem) => (
           <div
             key={saveListItem.listName}
-            className="border-t-[0.5px] border-t-[#6F6F6F] py-[22px]"
+            className="py-[11.97px] md:py-[22px] border-t-[#6F6F6F] border-t-[0.5px]"
           >
-            <div className="flex items-center justify-between">
-              <div className="flex justify-start">
-                <div className="rounded-sm border-[0.5px] border-[#D5D5D5] mr-2">
+            <div className="flex justify-between items-center">
+              <div className="flex justify-start items-center">
+                <div className="mr-2 border-[#D5D5D5] border-[0.5px] rounded-sm w-[20.45px] md:w-[37.5px] aspect-square">
                   <IconFlower
-                    size={IconSize.xl}
-                    className={getIconColor(saveListItem.iconColorId)}
+                    className={cn(
+                      getIconColor(saveListItem.iconColorId),
+                      'w-full h-full',
+                    )}
                   />
                 </div>
-                <div>
-                  <div>{saveListItem.listName}</div>
+                <div className="flex flex-col">
+                  <div className="text-[10px] md:text-lg">
+                    {saveListItem.listName}
+                  </div>
                   <div className="flex items-center text-[#6F6F6F]">
-                    <span>
-                      <IconPin
-                        size={IconSize.xs}
-                        className="text-[#6F6F6F] mr-2"
-                      />
+                    <span className="flex justify-center items-center mr-[4.35px] md:mr-2 h-[6.26px] md:h-[11.5px]">
+                      <IconPin className="w-full h-full text-[#6F6F6F]" />
                     </span>
-                    <span>{saveListItem.storeCount}</span>
+                    <span className="text-[8px] md:text-[14px]">
+                      {saveListItem.storeCount}
+                    </span>
                   </div>
                 </div>
               </div>
-              <button className="flex flex-col gap-2">
-                <div className="rounded-full w-1 h-1 bg-[#6F6F6F]"></div>
-                <div className="rounded-full w-1 h-1 bg-[#6F6F6F]"></div>
-                <div className="rounded-full w-1 h-1 bg-[#6F6F6F]"></div>
+              <button className="flex flex-col gap-[4.35px] md:gap-2">
+                <div className="bg-[#6F6F6F] rounded-full w-0.5 md:w-1 h-0.5 md:h-1"></div>
+                <div className="bg-[#6F6F6F] rounded-full w-0.5 md:w-1 h-0.5 md:h-1"></div>
+                <div className="bg-[#6F6F6F] rounded-full w-0.5 md:w-1 h-0.5 md:h-1"></div>
               </button>
             </div>
           </div>
