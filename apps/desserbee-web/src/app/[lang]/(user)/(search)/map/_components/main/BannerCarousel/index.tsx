504990c2a62b3ede9ecc217be750e70b7648acd9
eepy <eepy2.23@gmail.com>
2025-02-21 01:11:37 +0900

    fix(desserbee-web): 배너 캐러셀 반응형

--- apps/desserbee-web/src/app/[lang]/(user)/(search)/map/_components/main/BannerCarousel/index.tsx
+++ apps/desserbee-web/src/app/[lang]/(user)/(search)/map/_components/main/BannerCarousel/index.tsx
@@ -32,7 +32,7 @@ export function BannerCarousel() {
   return (
     <div className="relative">
       <Carousel
-        className="w-full overflow-x-hidden "
+        className="w-full overflow-hidden"
         opts={{
           align: 'start',
           containScroll: 'trimSnaps',
@@ -46,26 +46,26 @@ export function BannerCarousel() {
         ]}
         setApi={setApi}
       >
-        <CarouselContent className="mb-[13px] -ml-0">
+        <CarouselContent className="mb-[9px] md:mb-[13px] -ml-0">
           {BANNERS.map((banner) => (
             <CarouselItem
               className={cn(
                 banner.bgColor,
-                'relative flex justify-between items-center px-[27px] py-6 pl-0 rounded-base w-full',
+                'relative flex justify-between items-center pt-[13px] pb-[17px] md:pr-[27px] md:py-6 pl-0 rounded-base w-full',
               )}
               key={banner.content}
             >
               <div className="flex items-center">
-                <div className="mr-[18px] ml-6 w-fit flex justify-center items-center">
+                <div className="flex justify-center items-center mr-[9.51px] md:mr-[18px] ml-2 md:ml-6 w-[33.49px] md:w-[69px] h-[33px] md:h-[68px]">
                   <Image src={banner.imgSrc} alt={banner.content} />
                 </div>
-                <div className="font-semibold text-[#393939] text-3xl text-nowrap -tracking-[3%] whitespace-pre-line">
+                <div className="font-semibold text-[#393939] text-base md:text-3xl text-nowrap -tracking-[3%] whitespace-pre-line">
                   {banner.content}
                 </div>
               </div>
               <Link
                 href={banner.path}
-                className="right-[15px] bottom-[15px] absolute flex justify-center items-center bg-[#AA6120] px-[27px] py-[9px] rounded-[100px] h-11 font-semibold text-[22px] text-nowrap text-white"
+                className="right-[15px] bottom-[15px] absolute flex justify-center items-center bg-[#AA6120] px-[12.88px] md:px-[27px] py-[3.68px] md:py-[9px] rounded-[100px] font-semibold text-[10.12px] text-white md:text-[22px] text-nowrap"
               >
                 {banner.btnContent}
               </Link>
@@ -73,12 +73,12 @@ export function BannerCarousel() {
           ))}
         </CarouselContent>
       </Carousel>
-      <div className="absolute flex bottom-6 left-1/2 gap-2">
+      <div className="bottom-4 md:bottom-6 left-1/2 absolute flex gap-2 -translate-x-1/2 transform">
         {BANNERS.map((_, index) => (
           <div
             key={index}
             className={cn(
-              ' w-2 h-2 rounded-full',
+              'w-[3.77px] h-[3.77px] md:w-2 md:h-2 rounded-full',
               index === current ? 'bg-[#714115]' : 'bg-[#DE8332]',
             )}
           ></div>
