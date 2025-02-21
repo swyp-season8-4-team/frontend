13dceb8f3657b4ca6a4cb36892856c20c849b8db
eepy <eepy2.23@gmail.com>
2025-02-21 21:09:03 +0900

    chore(deploy): proxy 테스트

--- packages/infrastructures/src/repositories/apiRepository.ts
+++ packages/infrastructures/src/repositories/apiRepository.ts
@@ -1,5 +1,6 @@
 export default abstract class APIRepository {
   protected get endpoint(): string {
-    return 'http://localhost:3000/api'; // TODO: 환경변수 설정
+    // return 'http://localhost:3000/api'; // TODO: 환경변수 설정
+    return 'https://desserbee.com/api' as string;
   }
 }
