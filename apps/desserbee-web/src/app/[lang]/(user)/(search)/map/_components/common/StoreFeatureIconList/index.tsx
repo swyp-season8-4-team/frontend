6d3f2a6aa03f383a1cc52f78972cab0799322ef5
eepy <eepy2.23@gmail.com>
2025-02-23 08:41:51 +0900

    fix(desserbee-web): feature 아이콘 툴팁 수정

--- apps/desserbee-web/src/app/[lang]/(user)/(search)/map/_components/common/StoreFeatureIconList/index.tsx
+++ apps/desserbee-web/src/app/[lang]/(user)/(search)/map/_components/common/StoreFeatureIconList/index.tsx
@@ -2,7 +2,7 @@
 import IconCar from '@repo/design-system/components/icons/IconCar';
 import IconDog from '@repo/design-system/components/icons/IconDog';
 import IconTumbler from '@repo/design-system/components/icons/IconTumbler';
-import { useEffect, useMemo, useState } from 'react';
+import { useEffect, useMemo, useState, useRef } from 'react';
 
 interface StoreFeatureIconListProps {
   animalYn: boolean;
@@ -43,9 +43,7 @@ export function StoreFeatureIconList({
   const [selectedIconIndex, setSelectedIconIndex] = useState<number | null>(
     null,
   );
-  const [timeoutId, setTimeoutId] = useState<ReturnType<
-    typeof setTimeout
-  > | null>(null);
+  const timeoutRef = useRef<NodeJS.Timeout | null>(null);
 
   const handleIconClick = (index: number) => {
     setSelectedIconIndex(selectedIconIndex === index ? null : index);
@@ -53,24 +51,22 @@ export function StoreFeatureIconList({
 
   useEffect(() => {
     if (selectedIconIndex !== null) {
-      if (timeoutId) {
-        clearTimeout(timeoutId);
+      if (timeoutRef.current) {
+        clearTimeout(timeoutRef.current);
       }
 
-      const newTimeoutId = setTimeout(() => {
+      timeoutRef.current = setTimeout(() => {
         setSelectedIconIndex(null);
       }, 1500);
-
-      setTimeoutId(newTimeoutId);
     }
 
     return () => {
-      if (timeoutId) {
-        clearTimeout(timeoutId);
-        setTimeoutId(null);
+      if (timeoutRef.current) {
+        clearTimeout(timeoutRef.current);
+        timeoutRef.current = null;
       }
     };
-  }, [selectedIconIndex, timeoutId]);
+  }, [selectedIconIndex]);
 
   const featureList = useMemo(
     () => createFeatureList(parkingYn, tumblerYn, animalYn),
@@ -90,7 +86,7 @@ export function StoreFeatureIconList({
                 {feature.icon}
               </div>
               {selectedIconIndex === index && (
-                <div className="top-12 before:top-[-6px] left-1/2 before:left-1/2 z-10 absolute before:absolute bg-[#E8E8E8] px-[10px] py-1 before:border-r-[6px] before:border-r-transparent before:border-b-[6px] before:border-b-[#E8E8E8] before:border-l-[6px] before:border-l-transparent rounded-[5px] text-sm text-nowrap before:content-[''] -translate-x-1/2 before:-translate-x-1/2">
+                <div className="top-[22px] md:top-12 before:top-[-2px] md:before:top-[-5px] left-1/2 before:left-1/2 z-10 absolute before:absolute bg-[#E8E8E8] px-[6px] py-[5px] md:px-[10px] md:py-1 before:border-r-[3px] md:before:border-r-[6px] before:border-r-transparent before:border-b-[3px] md:before:border-b-[6px] before:border-b-[#E8E8E8] before:border-l-[3px] md:before:border-l-[6px] before:border-l-transparent rounded-[1.65px] md:rounded-[5px] text-[4px] md:text-sm text-nowrap before:content-[''] -translate-x-1/2 before:-translate-x-1/2 h-[5px] md:h-auto leading-[5px] md:leading-normal flex items-center justify-center">
                   {feature.tooltip}
                 </div>
               )}
