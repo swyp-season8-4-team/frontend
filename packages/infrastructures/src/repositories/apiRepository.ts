722fa2bafbbd09695e4a93ca49a738986902871f
eepy <eepy2.23@gmail.com>
2025-02-22 15:15:54 +0900

    fix: apiRepository endpoint

--- packages/infrastructures/src/repositories/apiRepository.ts
+++ packages/infrastructures/src/repositories/apiRepository.ts
@@ -1,5 +1,10 @@
+const origin =
+  process.env.NEXT_PUBLIC_APP_ENV !== 'prod'
+    ? 'http://localhost:3000'
+    : 'https://www.desserbee.com';
+
 export default abstract class APIRepository {
   protected get endpoint(): string {
-    return `${window.location.origin}/api`;
+    return `${origin}/api`;
   }
 }
