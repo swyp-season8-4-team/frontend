1d0218dc00528bc0bc7935083908d8369bbccd31
eepy <eepy2.23@gmail.com>
2025-02-24 05:22:54 +0900

    chore(desserbee-web): map/layout.tsx에 parallel routes 추가 (sidebar, bottomsheet)

--- apps/desserbee-web/src/app/[lang]/(user)/(search)/map/page.tsx
+++ apps/desserbee-web/src/app/[lang]/(user)/(search)/map/page.tsx
@@ -1,5 +1,5 @@
-import { KakaoMap } from './_components/main/KakaoMap';
-import { BannerCarousel } from './_components/main/BannerCarousel';
+import { KakaoMap } from './_components/KakaoMap';
+import { BannerCarousel } from './_components/BannerCarousel';
 
 import { CATEGORIES, USER_PREFERENCES } from './_consts/tag';
 
@@ -11,9 +11,6 @@ export default async function MapPage() {
   // 1. 선호도 태그 카테고리
   // 2. 유저 선호도 태그
   // 3. 유저가 저장한 가게목록
-  // 모두 KakaoMap으로 props 전달 - SideBar에 유저저장 가게목록 전달
-  // BottomSheet는 csr로?
-  // 상세페이지는 그냥 페이지 이동하는걸로? 아니면 interceptiong으로.
 
   // const storeService = new StoreService({
   //   storeRepository: new StoreAPIReopository(),
