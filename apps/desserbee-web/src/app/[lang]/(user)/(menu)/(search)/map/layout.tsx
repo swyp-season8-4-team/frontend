e88a21a198dfb81ababe7f7be76f4292ccc63937
eepy <eepy2.23@gmail.com>
2025-02-24 05:22:54 +0900

    chore(desserbee-web): map/layout.tsx에 parallel routes 추가 (sidebar, bottomsheet)

--- apps/desserbee-web/src/app/[lang]/(user)/(menu)/(search)/map/layout.tsx
+++ apps/desserbee-web/src/app/[lang]/(user)/(menu)/(search)/map/layout.tsx
@@ -1,15 +1,31 @@
 import { headers } from 'next/headers';
 
 export default async function MapLayout({
+  bottomSheet,
+  sidebar,
   children,
 }: {
+  bottomSheet: React.ReactNode;
+  sidebar: React.ReactNode;
   children: React.ReactNode;
 }) {
   const headerList = await headers();
   const authorization = headerList.get('authorization');
   if (!authorization) {
-    return <div>{children}</div>;
+    return (
+      <div>
+        {bottomSheet}
+        {sidebar}
+        {children}
+      </div>
+    );
   }
 
-  return <div>{children}</div>;
+  return (
+    <div>
+      {bottomSheet}
+      {sidebar}
+      {children}
+    </div>
+  );
 }
