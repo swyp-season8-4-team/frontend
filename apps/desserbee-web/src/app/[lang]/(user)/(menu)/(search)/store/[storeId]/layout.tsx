0c9ea61e1a043e5e38ba3ac833944590c22b3b6e
eepy <eepy2.23@gmail.com>
2025-02-26 12:13:24 +0900

    fix:deploy 에러

--- apps/desserbee-web/src/app/[lang]/(user)/(menu)/(search)/store/[storeId]/layout.tsx
+++ apps/desserbee-web/src/app/[lang]/(user)/(menu)/(search)/store/[storeId]/layout.tsx
@@ -1,15 +1,7 @@
 import type { WithChildren } from '@repo/ui/index';
 import type React from 'react';
 
-interface StoreDetailLayoutProps extends WithChildren {
-  detail: React.ReactNode;
-  tabs: React.ReactNode;
-  params: Promise<{ storeId: string }>;
-}
-
-export default function StoreDetailLayout({
-  children,
-}: StoreDetailLayoutProps) {
+export default function StoreDetailLayout({ children }: WithChildren) {
   return (
     <div className="top-0 z-30 absolute flex flex-col bg-white w-full min-h-[100dvh]">
       {children}
