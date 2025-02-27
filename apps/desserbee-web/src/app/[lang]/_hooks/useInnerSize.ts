4f0ec089d17f430bca2966fd764f9ecb8a1de54c
SeungHoon Jeong <seunghoon7561@gmail.com>
2025-02-27 16:35:41 +0900

    [desserbee-web] feat: mobile screen container (#96)
    Co-authored-by: hans <hans@hanss-MacBook-Pro.local>

---
+++ apps/desserbee-web/src/app/[lang]/_hooks/useInnerSize.ts
@@ -0,0 +1,23 @@
+'use client';
+
+import { useEffect, useState } from 'react'
+
+export const useInnerSize = () => {
+  const [innerHeight, setInnerHeight] = useState(0);
+  const [innerWidth, setInnerWidth] = useState(0);
+
+  useEffect(() => {
+    if (innerHeight !== window.innerHeight) {
+      setInnerHeight(window.innerHeight);
+    }
+    
+    if (innerWidth !== window.innerWidth) {
+      setInnerWidth(window.innerWidth);
+    }
+  }, [innerHeight, innerWidth]);
+
+  return {
+    innerHeight,
+    innerWidth,
+  };
+}
