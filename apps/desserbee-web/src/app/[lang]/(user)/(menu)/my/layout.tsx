aff19c6f62ed6c7e00abb42668cd8d805fbf6151
hans <hans@hanss-MacBook-Pro.local>
2025-03-01 16:03:52 +0900

    fix: my page unauth redirect

--- apps/desserbee-web/src/app/[lang]/(user)/(menu)/my/layout.tsx
+++ apps/desserbee-web/src/app/[lang]/(user)/(menu)/my/layout.tsx
@@ -1,19 +1,20 @@
+import { NavigationPathname } from '@repo/entity/src/navigation';
 import AuthNextAppRouteRepository from '@repo/infrastructures/src/repositories/authNextAppRouteRepository';
 import type { WithChildren } from '@repo/ui/index';
 import AuthService from '@repo/usecase/src/authService';
-import NavigationContainer from './(search)/_components/NavigationContainer';
+import { redirect } from 'next/navigation';
 
 const authService = new AuthService({
   authRepository: new AuthNextAppRouteRepository(),
-})
+});
 
-export default async function UserMenuLayout({ children }: WithChildren) {
+export default async function MyLayout({ children }: WithChildren) {
   const authorization = await authService.getAuthorization();
 
-  return (
-    <div className="bg-page h-[100dvh] text-default">
-      {children}
-      <NavigationContainer isAuthorized={!!authorization} />
-    </div>
-  );
+  if (!authorization) {
+    redirect(NavigationPathname.SignIn);
+  }
+  
+  return children;
+  
 }
