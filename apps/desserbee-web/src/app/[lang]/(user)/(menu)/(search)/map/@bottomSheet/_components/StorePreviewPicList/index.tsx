c411678e9fede3ae2844a0d786456508ed018ea8
eepy <eepy2.23@gmail.com>
2025-03-02 23:41:00 +0900

    [desserbee-web] 지도 쪽 수정 (#129)
    * feat: 리스트 문제
    
    * fix:
    
    * fix: 리뷰 작성 후 돌아가기
    
    * feat: 검색창 바텀시트
    
    * feat: 리스트 상세
    
    * fix: 리스트 가게 삭제
    
    * fix: 리스트 메인 수정
    
    * fix: 바텀시트 사진
    
    * feature/map-searc
    
    * fix: delete 버튼 활성화

--- apps/desserbee-web/src/app/[lang]/(user)/(menu)/(search)/map/@bottomSheet/_components/StorePreviewPicList/index.tsx
+++ apps/desserbee-web/src/app/[lang]/(user)/(menu)/(search)/map/@bottomSheet/_components/StorePreviewPicList/index.tsx
@@ -6,11 +6,16 @@ type StorePreviewPicListProps = Pick<StoreSummaryInfoData, 'ownerPickImages'>;
 export function StorePreviewPicList({
   ownerPickImages,
 }: StorePreviewPicListProps) {
-  if (!ownerPickImages) return;
+  if (!ownerPickImages) return null;
+
+  // 최대 4개의 이미지만 사용
+  const displayImages = ownerPickImages.slice(0, 4);
+  // 4개에서 모자란 수 계산
+  const emptySlots = 4 - displayImages.length;
 
   return (
     <div className="flex gap-[9.4px] md:gap-[22px]">
-      {ownerPickImages.map((image) => (
+      {displayImages.map((image) => (
         <div key={image} className="w-full aspect-square">
           <Image
             className="w-full h-full object-cover"
@@ -20,6 +25,13 @@ export function StorePreviewPicList({
             height={162}
           />
         </div>
+      ))}
+
+      {Array.from({ length: emptySlots }).map((_, index) => (
+        <div
+          key={`empty-${index}`}
+          className="w-full aspect-square bg-gray-100"
+        />
       ))}
     </div>
   );
