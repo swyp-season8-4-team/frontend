adb8fdbd024f0978d2e091acca2f266202c5df26
SeungHoon Jeong <seunghoon7561@gmail.com>
2025-03-05 03:18:28 +0900

    [desserbee-web] feat: review entity, usecase (#133)
    Co-authored-by: hans <hans@hanss-MacBook-Pro.local>

--- packages/infrastructures/src/repositories/reviewAPIRepository.ts
+++ packages/infrastructures/src/repositories/reviewAPIRepository.ts
@@ -1,19 +1,83 @@
+import fetch from '@repo/api/src/fetch';
+import type { BaseRequestData } from '@repo/entity/src/appMetadata';
 import type {
   CreateOnelineReviewRequestFormData,
   CreateOnelineReviewResponse,
-  OnelineReviewRequests,
   ReviewRepository,
+  ReviewUpdateData,
   StoreOnelineReivewData,
-  StoreOnelineReivewRequest,
+  StoreOnelineReivewRequest
 } from '@repo/entity/src/review';
 import APIRepository from './apiRepository';
-import type { BaseRequestData } from '@repo/entity/src/appMetadata';
-import fetch from '@repo/api/src/fetch';
 
 export default class ReviewAPIRepository
   extends APIRepository
   implements ReviewRepository
 {
+  async getMine(data: BaseRequestData<unknown>): Promise<unknown> {
+    if (!data) {
+      throw new Error('data is not set');
+    }
+
+    const url = `${this.endpoint}/reviews/me`;
+
+    const response = await fetch<unknown, unknown>({
+      method: 'GET',
+      url: url,
+    });
+
+    return response;
+  }
+
+  async edit({ data }: BaseRequestData<ReviewUpdateData>): Promise<unknown> {
+    if (!data) {
+      throw new Error('data is not set');
+    }
+
+    const { id } = data;
+
+    const url = `${this.endpoint}/reviews/${id}`;
+
+    const response = await fetch<unknown, unknown>({
+      method: 'PATCH',
+      url: url,
+    });
+
+    return response;
+  }
+
+  async delete({ data }: BaseRequestData<ReviewUpdateData>): Promise<unknown> {
+    if (!data) {
+      throw new Error('data is not set');
+    }
+
+    const { id } = data;
+
+    const url = `${this.endpoint}/reviews/${id}`;
+
+    const response = await fetch<unknown, unknown>({
+      method: 'DELETE',
+      url: url,
+    });
+
+    return response;
+  }
+
+  async getAll(data: BaseRequestData<unknown>): Promise<unknown> {
+    if (!data) {
+      throw new Error('data is not set');
+    }
+
+    const url = `${this.endpoint}/reviews`;
+
+    const response = await fetch<unknown, unknown>({
+      method: 'GET',
+      url: url,
+    });
+
+    return response;
+  }
+
   async getStoreOnelineReviews({
     data,
   }: BaseRequestData<StoreOnelineReivewRequest>): Promise<
