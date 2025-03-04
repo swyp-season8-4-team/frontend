667dfa67873a4790c755dff85e85b205af310c5e
hans <hans@hanss-MacBook-Pro.local>
2025-03-05 03:16:03 +0900

    [desserbee-web] feat: review entity, usecase

--- packages/entity/src/review.ts
+++ packages/entity/src/review.ts
@@ -52,6 +52,10 @@ export interface StoreOnelineReivewData {
   images: string[];
 }
 
+export interface ReviewUpdateData {
+  id: string;
+}
+
 export interface ReviewRepository {
   getStoreOnelineReviews(
     data: BaseRequestData<StoreOnelineReivewRequest>,
@@ -62,4 +66,16 @@ export interface ReviewRepository {
   }: BaseRequestData<CreateOnelineReviewRequestFormData>): Promise<
     CreateOnelineReviewResponse[]
   >;
+
+  // 내가 쓴 리뷰 조회
+  getMine(data: BaseRequestData<unknown>): Promise<unknown>;
+
+  // 내가 쓴 리뷰 수정
+  edit(data: BaseRequestData<ReviewUpdateData>): Promise<unknown>;
+
+  // 내가 쓴 리뷰 삭제
+  delete(data: BaseRequestData<ReviewUpdateData>): Promise<unknown>;
+
+  // 모든 리뷰 조회
+  getAll(data: BaseRequestData<unknown>): Promise<unknown>;
 }
