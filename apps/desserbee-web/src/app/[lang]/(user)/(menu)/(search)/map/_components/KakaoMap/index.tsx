4c53b274e45283eb291f6fb44296c35d7c9d4416
eepy <eepy2.23@gmail.com>
2025-03-05 01:34:33 +0900

    fix(map): 현재 위치로 돌아왔을 때도 새로 가게 업데이트

--- apps/desserbee-web/src/app/[lang]/(user)/(menu)/(search)/map/_components/KakaoMap/index.tsx
+++ apps/desserbee-web/src/app/[lang]/(user)/(menu)/(search)/map/_components/KakaoMap/index.tsx
@@ -475,6 +475,7 @@ export function KakaoMap({ preferenceCategories }: KakaoMapProps) {
   const handleMoveToCurrentPosition = useCallback(() => {
     if (servicesRef.current.mapService && isMapLoaded) {
       servicesRef.current.mapService.setMapCenter(currentPosition);
+      setIsFetchRequired(true);
     }
   }, [isMapLoaded, currentPosition]);
 
