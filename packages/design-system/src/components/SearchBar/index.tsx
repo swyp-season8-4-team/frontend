f20a9adec2f8ddcd638c19c9cbb2c05cd73d870d
eepy <eepy2.23@gmail.com>
2025-03-08 00:59:09 +0900

    fix: 검색 로직 단순화

--- packages/design-system/src/components/SearchBar/index.tsx
+++ packages/design-system/src/components/SearchBar/index.tsx
@@ -4,64 +4,29 @@ import { memo, useCallback } from 'react';
 
 interface SearchBarProps {
   searchTerm: string;
-  placeHolder: string;
   onChange: (value: string) => void;
-  onSearch?: (value: string) => void;
 }
 
 export const SearchBar = memo(function SearchBar({
   searchTerm,
-  placeHolder,
   onChange,
-  onSearch,
 }: SearchBarProps) {
-  const debouncedSetSearchTerm = useCallback(
-    (value: string) => {
-      debounce({
-        key: 'searchBarInput',
-        wait: 300,
-        callback: () => onChange(value),
-      });
-    },
-    [onChange],
-  );
-
   const handleChange = useCallback(
     (e: React.ChangeEvent<HTMLInputElement>) => {
       const value = e.target.value;
       onChange(value);
-      debouncedSetSearchTerm(value);
     },
-    [onChange, debouncedSetSearchTerm],
-  );
-
-  const handleKeyDown = useCallback(
-    (e: React.KeyboardEvent<HTMLInputElement>) => {
-      if (e.key === 'Enter' && onSearch) {
-        e.preventDefault();
-        onSearch(searchTerm);
-      }
-    },
-    [onSearch, searchTerm],
-  );
-
-  const handleSubmit = useCallback(
-    (e: React.FormEvent<HTMLFormElement>) => {
-      e.preventDefault();
-      onSearch?.(searchTerm);
-    },
-    [onSearch, searchTerm],
+    [onChange],
   );
 
   return (
-    <form className="relative px-4 w-full" onSubmit={handleSubmit}>
+    <form className="relative px-4 w-full">
       <input
         type="text"
+        placeholder="원하는 디저트 메이트를 검색해보세요!"
         value={searchTerm}
         className="shadow-[2px_2px_5px_0px_rgba(0,0,0,0.1)] my-[9px] py-[9.17px] pr-[20px] pl-[42.32px] rounded-[60px] w-full md:text-[15px] text-sm"
-        placeholder={placeHolder}
         onChange={handleChange}
-        onKeyDown={handleKeyDown}
       />
       <div className="top-1/2 left-[38px] md:left-[37.49px] absolute -translate-y-1/2">
         <IconSearch className="w-4 md:w-4 h-4 md:h-6 text-[#B4B4B4]" />
