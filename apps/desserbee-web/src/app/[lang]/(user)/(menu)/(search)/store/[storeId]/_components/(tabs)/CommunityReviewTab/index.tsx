3e68403665e11e7a4e0781bd3bcef518424c859f
eepy <eepy2.23@gmail.com>
2025-03-09 18:41:48 +0900

    fix: 커뮤니티 리뷰 디자인 수정

--- apps/desserbee-web/src/app/[lang]/(user)/(menu)/(search)/store/[storeId]/_components/(tabs)/CommunityReviewTab/index.tsx
+++ apps/desserbee-web/src/app/[lang]/(user)/(menu)/(search)/store/[storeId]/_components/(tabs)/CommunityReviewTab/index.tsx
@@ -3,8 +3,6 @@ import Image from 'next/image';
 import { useRouter } from 'next/navigation';
 import bee from '@/app/[lang]/(user)/(menu)/(search)/map/_assets/svg/logo-bee.svg';
 import type { StoreDetailInfoData } from '@repo/entity/src/store';
-import { formatDate } from '../../../../_utils/date';
-
 interface CommunityReviewTabProps {
   communityReviews: StoreDetailInfoData['communityReviews'];
 }
@@ -18,60 +16,51 @@ export function CommunityReviewTab({
   return (
     <div className="w-full">
       <div className="text-xs font-semibold md:text-lg">커뮤니티 리뷰</div>
-      {communityReviews.length > 0 ? (
-        communityReviews.map((review) => (
-          <div
-            key={review.reviewUuid}
-            className="flex justify-start items-center bg-[#F6F6F6] p-[8px] md:px-[14px] md:py-3 w-full"
-          >
-            <div className="bg-[#D2D2D2] flex-shrink-0 rounded-[1px] md:rounded-[3px] w-[21px] md:w-[147px] h-full aspect-square md:aspect-[147/94] overflow-hidden">
-              <Image
-                src={review.thumbnail}
-                className="object-cover w-full h-full"
-                alt="커뮤니티 리뷰 썸네일"
-              />
+      <div className="flex items-center bg-[#F6F6F6] p-[8px] md:px-[14px] md:py-3 w-full">
+        <div className="bg-[#D2D2D2] flex-shrink-0 rounded-[1px] md:rounded-[3px] w-[21px] md:w-[147px] aspect-square md:h-auto md:aspect-[147/94] overflow-hidden self-stretch">
+          <Image
+            src={bee}
+            className="object-contain w-full h-full"
+            alt="커뮤니티 리뷰 사진"
+          />
+        </div>
+        <div className="flex flex-col justify-center h-full items-start pl-[3px] md:pl-[11px] leading-[50%] md:leading-[130%]">
+          <div className="flex flex-col justify-center gap-[1px]">
+            <div className="flex items-center w-full justify-between">
+              <div className="flex  flex-shrink-0 items-center gap-x-[1px] gap-y-1 md:gap-x-[5px] md:gap-y-[7px]">
+                <div className="rounded-full bg-[#dadada] w-[8px] md:w-5 aspect-square overflow-hidden">
+                  <Image
+                    src={bee}
+                    className="object-cover w-full h-full"
+                    alt="프로필사진"
+                  />
+                </div>
+                <div className="text-[8px] md:text-base">닉네임</div>
+              </div>
+              <div className="flex w-full justify-end">
+                <div className="text-[8px] md:text-base">25.01.23</div>
+              </div>
             </div>
-            <div className="flex justify-between items-center w-full">
-              <div className="flex flex-col items-start pl-[3px] md:pl-[11px] leading-[50%] md:leading-[130%]">
-                <div className="flex flex-col gap-[1px]">
-                  <div className="flex items-center gap-x-[1px] gap-y-1 md:gap-x-[5px] md:gap-y-[7px]">
-                    <div className="rounded-full bg-[#dadada] w-[8px] md:w-5 aspect-square overflow-hidden">
-                      <Image
-                        src={review.profileImage}
-                        className="object-cover w-full h-full"
-                        alt="프로필사진"
-                      />
-                    </div>
-                    <div className="text-[8px] md:text-base">
-                      {review.nickname}
-                    </div>
-                  </div>
-                  <div className="w-full flex justify-between">
-                    <div className="overflow-hidden ">
-                      <div className="text-[9px] md:text-lg font-semibold line-clamp-1">
-                        {review.title}
-                      </div>
-                    </div>
-                  </div>
-                  <div className="text-[9px] md:text-base line-clamp-1 md:line-clamp-2">
-                    {review.content}
-                  </div>
-                  <div className="flex w-full justify-end">
-                    <div className="text-[8px] md:text-base">
-                      {formatDate(review.createdAt)}
-                    </div>
-                  </div>
+
+            <div className="w-full flex justify-between">
+              <div className="overflow-hidden ">
+                <div className="text-[9px] md:text-lg font-semibold line-clamp-1">
+                  이 카페 푸딩이 정말 맛있어요! 커피랑 같이 먹으니까 더
+                  맛있네요.
                 </div>
               </div>
             </div>
+            <div className="text-[8px] md:text-base line-clamp-1 md:line-clamp-2">
+              이 카페 푸딩이 정말 맛있어요! 커피랑 같이 먹으니까 더 맛있네요.
+              친구들이랑 오기 좋은 곳 같아요. 분위기도 좋고 디저트도 맛있어서
+              자주 방문할 것 같습니다.
+            </div>
           </div>
-        ))
-      ) : (
-        <div className="w-full text-[10px] md:text-base text-center">
-          <div>해당 가게에 작성된 커뮤니티 리뷰가 없어요.</div>
         </div>
-      )}
-
+      </div>
+      {/* <div className="w-full text-[10px] md:text-base text-center">
+        <div>해당 가게에 작성된 커뮤니티 리뷰가 없어요.</div>
+      </div> */}
       <div className="flex w-full justify-end">
         <button
           className="text-[10px] md:text-base"
