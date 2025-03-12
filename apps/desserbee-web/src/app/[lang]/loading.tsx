5dc496034b34b93e4ec4ed979d6431c303ed5531
hans <hans@hanss-MacBook-Pro.local>
2025-03-12 23:01:37 +0900

    [desserbee-web] fix: loading spinner

--- apps/desserbee-web/src/app/[lang]/loading.tsx
+++ apps/desserbee-web/src/app/[lang]/loading.tsx
@@ -1,13 +1,16 @@
 import IconLoadingSpinner from '@repo/design-system/components/icons/IconLoadingSpinner';
+import { MobileScreenProvider } from './_contexts/MobileScreenProvider';
 
 export default async function Loading() {
   return (
-    <div className="flex h-screen w-screen items-center justify-center">
-      <IconLoadingSpinner
-        className="animate-spin"
-        size={104}
-        viewBox="0 0 104 104"
-      />
-    </div>
+    <MobileScreenProvider>
+      <main className="flex flex-col items-center justify-center h-full min-h-screen">
+        <IconLoadingSpinner
+          className="animate-spin"
+          size={104}
+          viewBox="0 0 104 104"
+        />
+      </main>
+    </MobileScreenProvider>
   );
 }
