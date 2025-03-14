19ebd2d197cb3dbcb459c5c3f4150f353cdeb274
eepy <eepy2.23@gmail.com>
2025-03-14 17:53:23 +0900

    [dessebee-web] 수정 (#192)
    * fix: 지도 페이지 배경 꽉 차지 않는 문제 수정
    
    * fix: 디저트 메이트 스크롤 안 생기는 문제 수정

--- apps/desserbee-web/src/app/[lang]/(user)/mate/[mateId]/page.tsx
+++ apps/desserbee-web/src/app/[lang]/(user)/mate/[mateId]/page.tsx
@@ -1,19 +1,19 @@
-import type { WithParams } from "@/app";
-import MateAPIRepository from "@repo/infrastructures/src/repositories/mateAPIRepository";
-import MateService from "@repo/usecase/src/mateService";
-import { notFound } from "next/navigation";
-import CommentForm from "./_components/CommentForm";
-import CurrentApplyList from "./_components/CurrentApplyList";
-import MatePostSection from "./_components/MatePostSection";
-import MyMateDetailSection from "./_components/MyMateDetailSection";
-import { MateDetailProvider } from "./_contexts/MateDetailContext";
-import MateCommentListSection from "./_components/MateCommentListSection";
-import AuthNextAppRouteRepository from "@repo/infrastructures/src/repositories/authNextAppRouteRepository";
+import type { WithParams } from '@/app';
+import MateAPIRepository from '@repo/infrastructures/src/repositories/mateAPIRepository';
+import MateService from '@repo/usecase/src/mateService';
+import { notFound } from 'next/navigation';
+import CommentForm from './_components/CommentForm';
+import CurrentApplyList from './_components/CurrentApplyList';
+import MatePostSection from './_components/MatePostSection';
+import MyMateDetailSection from './_components/MyMateDetailSection';
+import { MateDetailProvider } from './_contexts/MateDetailContext';
+import MateCommentListSection from './_components/MateCommentListSection';
+import AuthNextAppRouteRepository from '@repo/infrastructures/src/repositories/authNextAppRouteRepository';
 
 const mateService = new MateService({
   authRepository: new AuthNextAppRouteRepository(),
-  mateRepository: new MateAPIRepository(), 
-})
+  mateRepository: new MateAPIRepository(),
+});
 
 export default async function MateDetailPage({ params }: WithParams) {
   const { mateId } = await params;
@@ -35,20 +35,20 @@ export default async function MateDetailPage({ params }: WithParams) {
     notFound();
   }
 
-  const waitList = waitListResult.status === 'fulfilled' ? waitListResult.value : [];
+  const waitList =
+    waitListResult.status === 'fulfilled' ? waitListResult.value : [];
 
   return (
-    <main className="flex flex-col h-[calc(100dvh - 52px)] px-4 gap-4 bg-[#f6f6f6]">
+    <main className="flex flex-col h-[calc(100dvh - 52px)] px-4 gap-4 bg-[#f6f6f6] overflow-y-auto ">
       <MateDetailProvider mate={mate}>
         <MatePostSection mate={mate} replyCount={waitList.length} />
-        
+
         <MyMateDetailSection mate={mate}>
           <div className="px-4">
             <CurrentApplyList waitList={waitList} />
           </div>
         </MyMateDetailSection>
-      
-      
+
         <MateCommentListSection mateId={mateId} />
         {mate.applyStatus === 'APPROVED' && <CommentForm />}
       </MateDetailProvider>
