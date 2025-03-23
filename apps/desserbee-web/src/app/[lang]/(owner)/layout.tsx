58504724469f65ca5ef195b8595450d4978fd1c1
eepy <eepy2.23@gmail.com>
2025-03-24 01:34:15 +0900

    fix: (owner) -> owner로 route 세그먼트 명시

--- apps/desserbee-web/src/app/[lang]/(owner)/layout.tsx
+++
@@ -1,3 +0,0 @@
-export default function OwnerLayout({ children }: { children: React.ReactNode }) {
-  return <>{children}</>;
-}
---
+++ apps/desserbee-web/src/app/[lang]/owner/layout.tsx
@@ -0,0 +1,7 @@
+export default function OwnerLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return <div className="h-[100dvh]">{children}</div>;
+}
