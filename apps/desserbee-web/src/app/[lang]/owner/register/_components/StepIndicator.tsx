5f57fc377df4d6904ccedd4a8527dbab9b72ad68
eepy <eepy2.23@gmail.com>
2025-04-01 02:02:19 +0900

    fix: 배포에러

--- apps/desserbee-web/src/app/[lang]/owner/register/_components/StepIndicator.tsx
+++ apps/desserbee-web/src/app/[lang]/owner/register/_components/StepIndicator.tsx
@@ -8,7 +8,7 @@ import { cn } from '@repo/ui/lib/utils';
 const STEP_TITLES = {
   [RegisterStep.BASIC_INFO]: '기본정보',
   [RegisterStep.MENU]: '메뉴등록',
-  [RegisterStep.CHECK]: '내용확인', //NOTICE: 내용 확인 갑자기 없어짐(논의 안된채로..) 또 논의안된채로 또 생길 수 있어서 남겨둠 (context에 주석처리되어있어서 현재 에러뜸)
+  // [RegisterStep.CHECK]: '내용확인', //NOTICE: 내용 확인 갑자기 없어짐(논의 안된채로..) 또 생길 수 있어서 남겨둠 (context에 주석처리되어있어서 현재 에러뜸)
   [RegisterStep.COMPLETE]: '등록완료',
 };
 
@@ -30,7 +30,7 @@ export default function StepIndicator() {
   const steps = [
     RegisterStep.BASIC_INFO,
     RegisterStep.MENU,
-    RegisterStep.CHECK,
+    // RegisterStep.CHECK,
     RegisterStep.COMPLETE,
   ];
 
