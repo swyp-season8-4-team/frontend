89ed7623057a8f7638482f6046e3a86390406dcc
eepy <eepy2.23@gmail.com>
2025-04-08 22:19:14 +0900

    fix: 회원가입 유도 버튼 재수정

--- apps/desserbee-web/src/app/[lang]/(auth)/(sign)/sign-in/_components/SignUpLink.tsx
+++ apps/desserbee-web/src/app/[lang]/(auth)/(sign)/sign-in/_components/SignUpLink.tsx
@@ -4,13 +4,13 @@ import { NavigationPathname } from '@repo/entity/src/navigation';
 export function SignUpLink() {
   const navigationService = new NavigationService({});
   return (
-    <div className="flex w-full items-center justify-center py-[34px] text-sm text-[#595959]">
-      <div>아직 계정이 없으신가요?&nbsp;</div>
+    <div className="text-neutral-30 flex w-full items-center justify-center gap-[11px] py-[34px] text-sm">
+      <div>아직 계정이 없으신가요?</div>
       <Link
         href={navigationService.getHref(NavigationPathname.SignUp)}
-        className="text-b-400 underline decoration-solid decoration-from-font underline-offset-auto"
+        className="text-secondary-30 text-b-400 font-medium underline decoration-solid decoration-from-font underline-offset-auto"
       >
-        일반 회원가입
+        회원가입
       </Link>
     </div>
   );
