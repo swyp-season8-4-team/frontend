8a615ee284ad8b767fc7b78e45c72a8d35aec285
eepy <eepy2.23@gmail.com>
2025-04-10 20:42:58 +0900

    fix: 디저트 메이트 게시글 북마크 토글 스타일 수정

--- apps/desserbee-web/src/app/[lang]/(user)/(with-navigation-bar)/community/dessert/mate/_components/CommunityMateCardBookmarkButton/index.tsx
+++ apps/desserbee-web/src/app/[lang]/(user)/(with-navigation-bar)/community/dessert/mate/_components/CommunityMateCardBookmarkButton/index.tsx
@@ -6,6 +6,7 @@ import { IconSize } from '@repo/design-system/components/icons';
 import IconBookmark from '@repo/design-system/components/icons/IconBookmark2';
 import { useCallback, useContext, useState } from 'react';
 import { saveMateBookmark, cancelMateBookmark } from './action';
+import { cn } from '@repo/ui/lib/utils';
 
 interface Props {
   mateId: string;
@@ -43,8 +44,10 @@ export default function CommunityMateCardBookmarkButton({ mateId }: Props) {
     <button onClick={handleClick}>
       <div className="h-3 w-3 md:h-4 md:w-4">
         <IconBookmark
-          className="h-full w-full text-black"
-          isSelected={isBookmarked}
+          className={cn(
+            'h-full w-full',
+            isBookmarked ? 'text-primary-80' : 'text-neutral-40',
+          )}
         />
       </div>
     </button>
