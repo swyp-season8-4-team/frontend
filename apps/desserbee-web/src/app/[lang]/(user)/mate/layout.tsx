bfad792beef4b93878a837cfe275848a64a879f3
eepy <eepy2.23@gmail.com>
2025-04-12 18:58:52 +0900

    fix: 디저트메이트 게시글 Image 컴포넌트 속성 적용 및 게시글 작성 스크롤 나오게 수정

--- apps/desserbee-web/src/app/[lang]/(user)/mate/layout.tsx
+++ apps/desserbee-web/src/app/[lang]/(user)/mate/layout.tsx
@@ -3,11 +3,11 @@ import BackButton from '../../_components/BackButton';
 
 export default async function MateLayout({ children }: WithChildren) {
   return (
-    <div className="flex flex-col h-[100dvh] bg-[#f6f6f6]">
-      <header className="flex items-center justify-between h-[52px] py-[18px]">
+    <div className="flex min-h-[100dvh] flex-col overflow-y-scroll bg-[#f6f6f6]">
+      <header className="flex h-[52px] items-center justify-between py-[18px]">
         <div className="flex items-center gap-[2px]">
           <BackButton />
-          <h1 className="text-[#6F6F6F] font-pretendard text-[18px] font-semibold leading-[130%] tracking-[-0.42px]">
+          <h1 className="font-pretendard text-[18px] font-semibold leading-[130%] tracking-[-0.42px] text-[#6F6F6F]">
             디저트 메이트
           </h1>
         </div>
