b13aa95e9e7798f23c305b863cce2966f696d385
eepy <eepy2.23@gmail.com>
2025-04-16 21:40:10 +0900

    fix: SignUpInput 컴포넌트 수정

--- packages/design-system/src/components/inputs/SignUp/index.tsx
+++ packages/design-system/src/components/inputs/SignUp/index.tsx
@@ -1,16 +1,34 @@
 import { cn } from '@repo/ui/lib/utils';
 import { forwardRef, type InputHTMLAttributes } from 'react';
 import { ResetButton } from '../../buttons/ResetButton';
+import IconEye from '../../icons/IconEye';
+import IconEyeBan from '../../icons/IconEyeBan';
 
 interface InputProps extends InputHTMLAttributes<HTMLInputElement> {
   error?: boolean;
   errorMessage?: string;
   onReset?: () => void;
   showReset?: boolean;
+  showPasswordToggle?: boolean;
+  onPasswordToggle?: () => void;
+  isPasswordVisible?: boolean;
 }
 
-export const SingUpInput = forwardRef<HTMLInputElement, InputProps>(
-  ({ error, errorMessage, className, onReset, showReset, ...props }, ref) => {
+export const SignUpInput = forwardRef<HTMLInputElement, InputProps>(
+  (
+    {
+      error,
+      errorMessage,
+      className,
+      onReset,
+      showReset,
+      showPasswordToggle,
+      onPasswordToggle,
+      isPasswordVisible,
+      ...props
+    },
+    ref,
+  ) => {
     return (
       <div className="relative">
         <input
@@ -22,13 +40,23 @@ export const SingUpInput = forwardRef<HTMLInputElement, InputProps>(
           )}
           {...props}
         />
-        <div className="absolute right-4 top-1/2 -translate-y-1/2">
+        <div className="absolute right-4 top-1/2 flex -translate-y-1/2 items-center gap-[11.5px]">
           <ResetButton
             onClick={onReset}
             isShown={showReset}
             height={18}
             width={18}
           />
+          {showPasswordToggle && (
+            <button
+              type="button"
+              tabIndex={-1}
+              onClick={onPasswordToggle}
+              className="text-neutral-30"
+            >
+              {isPasswordVisible ? <IconEye /> : <IconEyeBan />}
+            </button>
+          )}
         </div>
         {error && errorMessage && (
           <p className="absolute mt-1 text-sm text-[#FF3B30]">{errorMessage}</p>
@@ -38,4 +66,4 @@ export const SingUpInput = forwardRef<HTMLInputElement, InputProps>(
   },
 );
 
-SingUpInput.displayName = 'SingUpInput';
+SignUpInput.displayName = 'SignUpInput';
