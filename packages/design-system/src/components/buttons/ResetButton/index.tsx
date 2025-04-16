4105bbe3b245dee2f2d75df149709ef729c06d33
eepy <eepy2.23@gmail.com>
2025-04-16 21:06:41 +0900

    fix: ResetButton 수정

--- packages/design-system/src/components/buttons/ResetButton/index.tsx
+++ packages/design-system/src/components/buttons/ResetButton/index.tsx
@@ -1,13 +1,24 @@
 import IconXRound from '../../icons/IconXRound';
+
 interface ResetButtonProps {
   isShown?: boolean;
   onClick?: () => void;
+  height?: number;
+  width?: number;
 }
-export function ResetButton({ isShown = false, onClick }: ResetButtonProps) {
+
+const getContainerStyle = (h: number, w: number) => `h-[${h}px] w-[${w}px]`;
+
+export function ResetButton({
+  isShown = false,
+  onClick,
+  height = 15,
+  width = 15,
+}: ResetButtonProps) {
   if (!isShown) return null;
 
   return (
-    <div className="h-[15px] w-[15px]" onClick={onClick}>
+    <div className={getContainerStyle(height, width)} onClick={onClick}>
       <IconXRound className="h-full w-full cursor-pointer text-[#CDC8C3]" />
     </div>
   );
