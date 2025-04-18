bb066837591fe5a9bfcf32aa858fd0537b6b3a21
eepy <eepy2.23@gmail.com>
2025-04-19 00:15:55 +0900

    feat: AppRegisterUserExtraInfoForm 서버액션

---
+++ apps/desserbee-web/src/app/[lang]/(user)/owner/_components/AppRegisterUserExtraInfoForm/action.ts
@@ -0,0 +1,40 @@
+'use server';
+
+import { HTTPError } from '@repo/api/src/error';
+import UserAPIRepository from '@repo/infrastructures/src/repositories/userAPIRepository';
+import AuthAPIRepository from '@repo/infrastructures/src/repositories/authAPIRepository';
+import UserService from '@repo/usecase/src/userService';
+import type { User } from '@repo/entity/src/user';
+
+const userService = new UserService({
+  userRepository: new UserAPIRepository(),
+  authRepository: new AuthAPIRepository(),
+});
+
+interface updateUserInfoProps {
+  user: User;
+  name: string;
+  phoneNumber: string;
+}
+
+export const updateUserInfo = async ({
+  user,
+  name,
+  phoneNumber,
+}: updateUserInfoProps) => {
+  try {
+    const result = await userService.updateMe({
+      ...user,
+      name,
+      phoneNumber,
+      roles: ['ROLE_OWNER', 'ROLE_USER'],
+    });
+
+    return { success: true, data: result };
+  } catch (error) {
+    if (error instanceof HTTPError) {
+      console.log(error.data);
+    }
+    throw error;
+  }
+};
