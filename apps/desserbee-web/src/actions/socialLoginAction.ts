c5b2df0b1dd0358128bd9ee2bedb81d4d7852627
eepy <eepy2.23@gmail.com>
2025-04-19 14:36:34 +0900

    fix: 소셜 로그인 에러처리

--- apps/desserbee-web/src/actions/socialLoginAction.ts
+++ apps/desserbee-web/src/actions/socialLoginAction.ts
@@ -86,6 +86,7 @@ export default async function socialLoginAction({
   } catch (error) {
     if (error instanceof HTTPError) {
       console.log(error.data);
+      throw error;
     }
 
     throw error;
