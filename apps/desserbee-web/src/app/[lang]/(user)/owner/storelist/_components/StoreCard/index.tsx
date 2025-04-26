c96da4e5099f0dbb0b1f742c930ba53aa4c0801a
hyenee99 <twoddal2@naver.com>
2025-04-26 18:21:50 +0900

    feat: owner 가게 목록 api 연동

--- apps/desserbee-web/src/app/[lang]/(user)/owner/storelist/_components/StoreCard/index.tsx
+++ apps/desserbee-web/src/app/[lang]/(user)/owner/storelist/_components/StoreCard/index.tsx
@@ -1,14 +1,28 @@
+import Image from "next/image";
+
 interface StoreCardProps {
   img?: string;
   name: string;
   tag: string[];
   description: string;
 }
-export default function StoreCard({ name, tag, description }: StoreCardProps) {
+export default function StoreCard({ name, tag, description, img }: StoreCardProps) {
   return (
     <div className="m-auto h-[30vh] w-[95%] cursor-pointer">
-      <div className="h-[70%] w-full rounded-md">
-        사진
+      <div className="h-[70%] w-full rounded-md relative">
+        {img ? (
+          <Image 
+            src={img} 
+            alt={`${name} 이미지`} 
+            fill
+            sizes="100vw"
+            className="rounded-md object-cover" 
+          />
+        ) : (
+          <div className="h-full w-full rounded-md bg-gray-200 flex items-center justify-center">
+            사진 없음
+          </div>
+        )}
       </div>
       <div className="flex items-center justify-between">
         <p className="p-1 text-[20px] font-bold">{name}</p>
@@ -25,3 +39,4 @@ export default function StoreCard({ name, tag, description }: StoreCardProps) {
     </div>
   );
 }
+
