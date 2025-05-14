08d2da0c6072dccd90ed36ac40cb91b8333b6ab1
eepy <eepy2.23@gmail.com>
2025-05-14 18:40:46 +0900

    [desserbee-web] 모바일 웹에서 화면 확대 방지 (#255)
    * fix: 모바일 웹에서 화면 확대 안되게 수정
    
    * fix: revalidate 적용 메서드 수정
    
    * fix: 모바일웹 확대 방지 수정

--- apps/desserbee-web/src/app/[lang]/layout.tsx
+++ apps/desserbee-web/src/app/[lang]/layout.tsx
@@ -1,6 +1,6 @@
 import '@repo/ui/styles/globals.css';
 
-import type { Metadata } from 'next';
+import type { Metadata, Viewport } from 'next';
 import { Fragment } from 'react';
 
 import type { WithParams } from '@/app';
@@ -16,6 +16,14 @@ import { fontVariables } from '../fonts';
 import { cn } from '@repo/ui/lib/utils';
 
 const metadataService = new MetadataService();
+
+export const viewport: Viewport = {
+  width: 'device-width',
+  initialScale: 1,
+  maximumScale: 1,
+  minimumScale: 1,
+  userScalable: false,
+};
 
 export const metadata: Metadata = {
   title: metadataService.title,
